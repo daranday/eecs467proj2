f387670bca19d61e8f254723795f7975f633aa75
kevinong <kevinongs@gmail.com>
2015-03-17 00:25:48 -0400

    improved ai

--- src/a2/a2_ai.cpp
+++ src/a2/a2_ai.cpp
@@ -157,11 +157,32 @@ int AI::findNewMove() {
 int AI::findNewMoveHelper() {
 
     if(isBoardEmpty()) {
-        cout << "board is empty" << endl;
-        return 0;
+        // cout << "board is empty" << endl;
+        return 4;
     }
 
+    int redCount = 0, greenCount = 0;
+    for(int i = 0; i < 9; i++) {
+        if(board[i] == 'R') {
+            redCount++;
+        }
+        if(board[i] == 'G') {
+            greenCount++;
+        }
+    }
+
+    // if (redCount == 0 && greenCount == 1 && board[4] == '.') {
+    //     return 4;
+    // }
+
     int wins[8][3] = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+    int wins2[8][3];
+
+    for(int i = 0; i < 8; i++) {
+        for (int j = 0; j < 3; j++) {
+            wins2[i][j] = abs(wins[i][j] - 8);
+        }
+    }
 
     int move;
     int count;
@@ -176,7 +197,7 @@ int AI::findNewMoveHelper() {
                 count++;
             } else if (board[wins[i][j]] == '.') {
                 validMove = true;
-                move = wins[i][j];
+                move = wins2[i][j];
             }
         }
         if(count == 2 && validMove) {
@@ -193,13 +214,63 @@ int AI::findNewMoveHelper() {
                 count++;
             } else if (board[wins[i][j]] == '.') {
                 validMove = true;
-                move = wins[i][j];
+                move = wins2[i][j];
             }
         }
         if(count == 2 && validMove) {
             return move;
         }
     }
+
+    vector<int> possibleMoves;
+    vector<int>forks;
+    int forkCount;
+    for(int i = 0; i < 9; i++) {
+        if(board[i] == '.') {
+            possibleMoves.push_back(i);
+        }
+    }
+
+    for(int i = 0; i < possibleMoves.size(); i++) {
+        for(int j = 0; j < 8; j++) {
+            redCount = 0;
+            greenCount = 0;
+            forkCount = 0;
+            // cout << "--------" << endl;
+            if(wins[j][0] == possibleMoves[i] || wins[j][1] == possibleMoves[i] || wins[j][2] == possibleMoves[i]) {
+                // cout << "here" << endl;
+                for(int k = 0; k < 3; k++) {
+                    if(board[wins[j][k]] == 'R') {
+                        redCount++;
+                    }
+                    if(board[wins[j][k]] == 'G') {
+                        greenCount++;
+                    }
+                }
+
+                if(redCount == 1 && greenCount == 0) {
+                    forkCount++;
+                }
+            }
+        }
+        forks.push_back(forkCount);
+    }
+
+    int maxForkSoFar = 0;
+    int maxForkInd;
+    for(int i = 0; i < forks.size(); i++) {
+        if(forks[i] > maxForkSoFar) {
+            maxForkSoFar = forks[i];
+            maxForkInd = possibleMoves[i];
+        }
+    }
+
+    if(maxForkSoFar > 0) {
+        // cout << "max fork " << maxForkSoFar << endl;
+        return abs(maxForkInd - 8);
+    }
+
+
 
     int maxSoFar = 0;
     int adjCount;
@@ -224,49 +295,107 @@ int AI::findNewMoveHelper() {
             }
         }
 
-            if(count == 1 && emptyCount == 2) {
-                if(move == 0 || move == 2) {
-                    cout << "here" << endl;
-                    return wins[i][1];
-                }
-                if(move == 1) {
-                    coord2D1 = oneD2twoD(wins[i][0]);
-                    coord2D2 = oneD2twoD(wins[i][2]);
+        if(count == 1 && emptyCount == 2) {
+            if(move == 0 || move == 2) {
+                cout << "here" << endl;
+                return wins2[i][1];
+            }
+            if(move == 1) {
+                coord2D1 = oneD2twoD(wins[i][0]);
+                coord2D2 = oneD2twoD(wins[i][2]);
 
-                    adjCount1 = 0;
-                    adjCount2 = 0;
-                    for (int j = -1; j < 1; j++) {
-                        for (int k = -1; k < 1; k++) {
-                            if (j != 0 && k != 0) {
-                                if(isValidCoord2D(coord2D1.first + j, coord2D1.second + k)) {
-                                    if(board[twoD2oneD(coord2D1.first + j, coord2D1.second + k)] == 'R') {
-                                        adjCount1++;
-                                    }
+                adjCount1 = 0;
+                adjCount2 = 0;
+                for (int j = -1; j < 1; j++) {
+                    for (int k = -1; k < 1; k++) {
+                        if (j != 0 && k != 0) {
+                            if(isValidCoord2D(coord2D1.first + j, coord2D1.second + k)) {
+                                if(board[twoD2oneD(coord2D1.first + j, coord2D1.second + k)] == 'R') {
+                                    adjCount1++;
+                                }
 
-                                    if(board[twoD2oneD(coord2D2.first + j, coord2D2.second + k)] == 'R') {
-                                        adjCount2++;
-                                    }
-
+                                if(board[twoD2oneD(coord2D2.first + j, coord2D2.second + k)] == 'R') {
+                                    adjCount2++;
                                 }
 
                             }
+
                         }
                     }
-
-                    if(adjCount2 > adjCount1) {
-                        return wins[i][2];
-                    } else {
-                        return wins[i][0];
-                    }
-
                 }
+
+                if(adjCount2 > adjCount1) {
+                    // cout << "near red" << endl;
+                    return wins2[i][2];
+                } else if (adjCount1 > adjCount2) {
+                    // cout << "near red" << endl;
+                    return wins2[i][0];
+                }
+
             }
+        }
+        
+    }
+
+    for(int i = 0; i < 8; i++) {
+        count = 0;
+        emptyCount = 0;
+        validMove = false;
+        for(int j = 0; j < 3; j++) {
+            if(board[wins[i][j]] == 'G') {
+                move = j;
+                count++;
+            } else if (board[wins[i][j]] == '.') {
+                emptyCount++;
+            }
+        }
+
+        if(count == 1 && emptyCount == 2) {
+            if(move == 0 || move == 2) {
+                // cout << "here" << endl;
+                return wins2[i][1];
+            }
+            if(move == 1) {
+                coord2D1 = oneD2twoD(wins[i][0]);
+                coord2D2 = oneD2twoD(wins[i][2]);
+
+                adjCount1 = 0;
+                adjCount2 = 0;
+                for (int j = -1; j < 1; j++) {
+                    for (int k = -1; k < 1; k++) {
+                        if (j != 0 && k != 0) {
+                            if(isValidCoord2D(coord2D1.first + j, coord2D1.second + k)) {
+                                if(board[twoD2oneD(coord2D1.first + j, coord2D1.second + k)] == 'G') {
+                                    adjCount1++;
+                                }
+
+                                if(board[twoD2oneD(coord2D2.first + j, coord2D2.second + k)] == 'G') {
+                                    adjCount2++;
+                                }
+
+                            }
+
+                        }
+                    }
+                }
+
+                if(adjCount2 > adjCount1) {
+                    // cout << "near green" << endl;
+                    return wins2[i][2];
+                } else if (adjCount1 > adjCount2) {
+                    // cout << "near green" << endl;
+                    return wins2[i][0];
+                }
+
+            }
+        }
         
     }
 
     for (int i = 0; i < 9; i++) {
         if (board[i] == '.') {
-            return i;
+            // cout << "first empty" << endl;
+            return abs(i - 8);
         }
     }
 
@@ -282,16 +411,22 @@ int AI::findNewMoveHelper() {
 
 //     cout << "game starts" << endl;
 //     while(1) {
-//         A.checkEnd();
-//         A.aiPlay();
+//         if(A.checkEnd()) {
+//             return 0;
+//         }
+//         // A.aiPlay();
+//         A.oppPlay();
 
 //         A.printBoard();
-//         A.checkEnd();
+//         if(A.checkEnd()) {
+//             return 0;
+//         }
 
 //         // The following three lines will be replaced by receiving a new board
-//         A.oppPlay();
+//         // A.oppPlay();
+//         A.aiPlay();
 //         A.printBoard();
 //     }
 
 //     return 0;
-}
+// }
