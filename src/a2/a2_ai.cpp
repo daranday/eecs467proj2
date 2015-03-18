048575b28d0401830b089e992cff043fa8fb1224
kevinong <kevinongs@gmail.com>
2015-03-18 11:12:06 -0400

    fixed ai

--- src/a2/a2_ai.cpp
+++ src/a2/a2_ai.cpp
@@ -151,9 +151,9 @@ int AI::findNewMoveHelper() {
         }
     }
 
-    // if (redCount == 0 && greenCount == 1 && board[4] == '.') {
-    //     return 4;
-    // }
+    if (redCount == 0 && greenCount == 1 && board[4] == '.') {
+        return 4;
+    }
 
     int wins[8][3] = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
     int wins2[8][3];
